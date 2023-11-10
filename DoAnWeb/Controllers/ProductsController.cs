d26d9375e99595636f55da602c2bad38243026d8
HauPC10082003 <pch10082003@gmail.com>
2023-11-10 08:31:20 +0700

    Update

--- DoAnWeb/Controllers/ProductsController.cs
+++ DoAnWeb/Controllers/ProductsController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DoAnWeb.Data;
 using DoAnWeb.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DoAnWeb.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly DoAnWebContext _context;
